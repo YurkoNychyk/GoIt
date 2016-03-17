7f94c8312267be33d70ffd457c7c83d2939f2d36
YurkoNychyk <yurko.nychik@gmail.com>
2016-03-17 09:28:26 +0200

    array forming done

---
+++ module5/src/ArraySorting.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+/**
+ * Created by home on 17.03.2016.
+ */
+public class ArraySorting {
+    public static void main(String[] args) {
+        int[] newArray = new int[10];
+        Random randomizer = new Random();
+
+        for (int i=0; i< newArray.length; i++)  {
+            newArray[i]= randomizer.nextInt();
+            System.out.println(" " + newArray[i]);
+        }
+
+        public int[] sortArray (int[] array) {
+
+
+        }
+
+    }
+}
